266c27fdb2e2c0b45ae4cb4f275f2fbeab6a97a3
Eric Steele <esteele@plone.org>
2011-04-24 01:43:27 +0000

    More boilerplate.
    svn path=/plone.seleniumtesting/trunk/; revision=49077

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.2a1'
+
+setup(name='plone.seleniumtesting',
+      version=version,
+      description="Selenium Testing infrastructure for Plone projects.",
+      long_description=open("README.txt").read() + "\n\n" +
+                       open("CHANGES.txt").read() + "\n\n" +
+      classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        ],
+      keywords='plone zope testing',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.seleniumtesting',
+      license='GPL version 2',
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'unittest2',
+          'plone.app.testing'
+          'selenium',
+      ],
+      extras_require={
+      },
+      )
