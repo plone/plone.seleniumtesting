6d34383c2a34317a9f6363daa531ae9f97981b3f
Ed Manlove <devPyPlTw@verizon.net>
2011-09-17 14:38:03 +0000

    Reworked selenium server connection to use direct connection to webdriver. Moved page elements from class objects to instance objects.
    svn path=/plone.seleniumtesting/trunk/; revision=52065

--- plone/seleniumtesting/loginpage.py
+++ plone/seleniumtesting/loginpage.py
@@ -29,12 +29,13 @@ class StatusMessageTextElement(BaseTextElement):
 
 class LoginPage(BasePage):
     
-    username = UsernameElement()
-    password = PasswordElement()
-    status_message = StatusMessageTextElement()
-    
-    def __init__(self):
-        self.se = wrapper().connection
+    def __init__(self, selenium):
+        self.se = selenium
+
+        self.username = UsernameElement(self.se)
+        self.password = PasswordElement(self.se)
+        self.status_message = StatusMessageTextElement(self.se)
+        
         # TODO Make sure we really are on the login page
         # self.assertTrue("Forgot your password?" in self.se.get_page_source())
 
